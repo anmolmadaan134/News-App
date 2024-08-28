1c2ec9954a352bb72cd78558a133182e61ac1c61
anmolmadaan368 <[anmolmad503@gmail.com]>
2024-08-28 09:47:23 +0530

    image

--- News/src/Components/NewsItem.jsx
+++ News/src/Components/NewsItem.jsx
@@ -1,9 +1,10 @@
 
 
 export const NewsItem = ({title,description,src,url}) => {
+  const defaultImage = "https://cdn.pixabay.com/photo/2017/06/26/19/03/news-2444778_960_720.jpg"
   return (
     <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth:"345px"}}>
-  <img src={src} style ={{height:"200px",width:"325px"}} className="card-img-top" alt="..."/>
+  <img src={src || defaultImage} style ={{height:"200px",width:"325px"}} className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title.slice(0,50)}</h5>
     <p className="card-text">{description?description.slice(0,90):"News not available"}</p>
